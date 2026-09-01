a7a2ae12a6f483961cdcb8918cdbb5478787d648
Shaheen <shn.sdq29@gmail.com>
2026-09-01 23:10:23 +0530

    Created a new Hero section component with dynamic text and images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1>Welcome</h1>
-      </div>
-    </main>
+   <>
+   <Hero/>
+   </>
   )
 }
 
