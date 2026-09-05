624b4ad485770f37cb5316065181d82e08ae4312
Shaheen <shn.sdq29@gmail.com>
2026-09-05 22:26:49 +0530

    Fix typo in penumbra property and add AnimatedCounter component for displaying animated numbers

--- src/HeroModels/HeroLights.jsx
+++ src/HeroModels/HeroLights.jsx
@@ -7,28 +7,40 @@ const HeroLights = () => {
     <spotLight position={[2,5,6]}
     angle={0.15}
     intensity={100}
-    penubra={0.2}
+    penumbra={0.2}
     color="white"
     />
 
     <spotLight position={[4,5,4]}
     angle={0.3}
     intensity={40}
-    penubra={0.5}
+    penumbra={0.5}
     color="#4cc9f0"
     />
     <spotLight position={[-3,5,5]}
     angle={0.4}
     intensity={60}
-    penubra={1}
+    penumbra={1}
     color="#9d4edd"
     />
 
     <primitive
     object= {new THREE.RectAreaLight('#A259FF',8,3,2)}
-    position={[0,1,0]}
+    position={[1,3,4]}
     intensity={15}
     rotation={[-Math.PI / 4,Math.PI/4,0]} 
+    />
+
+    <pointLight 
+    position={[0,1,0]}
+    intensity={10}
+    color="#7209b7"
+    />
+
+    <pointLight 
+    position={[1,2,-2]}
+    intensity={10}
+    color="#0d00a4"
     />
 
     </>
