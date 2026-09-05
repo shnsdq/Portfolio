5f7c0a982c60f05b8a5e2c776f3eab81ed79c171
Shaheen <shn.sdq29@gmail.com>
2026-09-05 22:22:28 +0530

    Added GSAP animations for hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import { words } from '../constants'
 import Button from '../components/Button'
 import HeroExperience from '../HeroModels/HeroExperience'
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+import AnimatedCounter from '../components/AnimatedCounter';
 
 const Hero = () => {
+ useGSAP(()=>{
+  gsap.fromTo('.hero-text h1',
+    {
+      y:50,
+      opacity:0
+    },
+    {
+      y:0,
+      opacity:1,
+      stagger:0.2,
+      duration:1,
+      ease:'power2.inOut'
+    },
+  )
+ })
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className='absolute top-0 left-0 z-10'>
@@ -43,6 +62,7 @@ const Hero = () => {
                 text="See my work" />
             </div>
         </header>
+        
         {/*RIGHT: 3D MODEL  */}
         <figure>
           <div className='hero-3d-layout'>
@@ -50,6 +70,7 @@ const Hero = () => {
           </div>
         </figure>
       </div>
+      <AnimatedCounter/>
     </section>
   )
 }
