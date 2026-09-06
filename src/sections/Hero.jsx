e5e395675d433354c73f346fb1ae4dbab0a247a5
Shaheen <shn.sdq29@gmail.com>
2026-09-07 00:10:34 +0530

    Refactor import paths for HeroExperience component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { words } from '../constants'
 import Button from '../components/Button'
-import HeroExperience from '../HeroModels/HeroExperience'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 import {useGSAP} from '@gsap/react';
 import gsap from 'gsap';
 import AnimatedCounter from '../components/AnimatedCounter';
