ab375560f8804c805f0a86ca11c5f5f5b71470ff
Shaheen <shn.sdq29@gmail.com>
2026-09-06 23:58:32 +0530

    Added ShowcaseSection component for project display layout

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ShowcaseSection = () => {
+  return (
+    <div id="work" className='app-showcase'>
+        <div className='w-full'>
+            <div className='showcaselayout'>
+                {/* LEFT */}
+                <div className='first-project-wrapper'>
+                    <div className='image-wrapper'>
+
+                    </div>
+
+                </div>
+                {/* RIGHT */}
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ShowcaseSection
