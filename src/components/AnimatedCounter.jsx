d80bace40800cefaa4630e3db68c3acb28ba2775
Shaheen <shn.sdq29@gmail.com>
2026-09-06 23:58:10 +0530

    Added CountUp component for animated number display in AnimatedCounter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { counterItems } from '../constants'
+import CountUp from 'react-countup';
 
 const AnimatedCounter = () => {
   return (
@@ -10,6 +11,7 @@ const AnimatedCounter = () => {
 
                 <div key={counterItems.label} className='counter-number text-white text-5xl font-bold mb-2'>
                  0{item.suffix}
+                 <CountUp suffix={item.suffix} end={item.value} />
                 </div>
                 <div className='text-white-50 text-lg'>{item.label}</div>
                 </div>
